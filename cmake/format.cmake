16fc15efd618977e3719768c12dd496df89e0e62
Sriram Katta <sriram.katta@fau.com>
2025-07-17 10:17:01 +0200

    [FIX] Correct package name casing in format.cmake

--- cmake/format.cmake
+++ cmake/format.cmake
@@ -1,12 +1,5 @@
 CPMAddPackage(
-  NAME Format.cmake
+  NAME format.cmake
   VERSION 1.8.2
-  GITHUB_REPOSITORY TheLartians/Format.cmake
-  OPTIONS # set to yes skip cmake formatting
-          "FORMAT_SKIP_CMAKE NO"
-          # set to yes skip clang formatting
-          "FORMAT_SKIP_CLANG NO"
-          # path to exclude (optional, supports regular expressions) "CMAKE_FORMAT_EXCLUDE
-          # cmake/CPM.cmake" extra arguments for cmake_format (optional) "CMAKE_FORMAT_EXTRA_ARGS -c
-          # /path/to/cmake-format.{yaml,json,py}"
+  GITHUB_REPOSITORY "TheLartians/Format.cmake"
 )
