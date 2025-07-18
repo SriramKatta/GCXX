b276efd9462b0e9372a2a8c1f5406a71b9ddeabc
Sriram Katta <sriram.katta@fau.com>
2025-07-19 01:34:52 +0200

    [ADD] Implement backend headers for CUDA and HIP support

---
+++ lib/include/gpucxx/backend/backend.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef GPUCXX_API_RUNTIME_BACKEND_HPP
+#define GPUCXX_API_RUNTIME_BACKEND_HPP
+
+// clang-format off
+#ifdef GPUCXX_CUDA_MODE
+    #include <gpucxx/backend/cuda_backend.hpp>
+#elif GPUCXX_HIP_MODE
+    #include <gpucxx/backend/hip_backend.hpp>
+#endif
+
+#define STRINGIFY(x)                #x
+#define TOSTRING(x)                 STRINGIFY(x)
+#define STRINGIFY_AND_APPEND(a, b)  a##b
+#define APPEND_NAME(a, b)           STRINGIFY_AND_APPEND(a, b)
+
+#define        GPUCXX_RUNTIME_BACKEND(name)    APPEND_NAME(RUNTIME_BACKEND, name)
+constexpr auto GPUCXX_RUNTIME_BACKEND_STR    = TOSTRING(RUNTIME_BACKEND);
+// clang-format on
+
+#endif
---
+++ lib/include/gpucxx/backend/cuda_backend.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef CUDA_BACKEND_HPP
+#define CUDA_BACKEND_HPP
+
+#include <cuda_runtime.h>
+
+#define RUNTIME_BACKEND cuda
+
+
+#endif
