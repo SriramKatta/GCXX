c2873e5997d4601bba71c6b0f1ba53b5036e271f
Sriram Katta <sriram.katta@fau.com>
2025-07-19 01:35:05 +0200

    [ADD] Create api.hpp header for GPUCXX API wrappers and error handling

---
+++ lib/include/gpucxx/api.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef GPUCXX_API_WRAPPERS_HPP
+#define GPUCXX_API_WRAPPERS_HPP
+
+#include <gpucxx/backend/backend.hpp>
+// Section for diffrent error handlings
+// for example runtime, blas, etc.
+#include <gpucxx/error/runtime_error.hpp>
+
+
+// Section for API
+
+#endif
