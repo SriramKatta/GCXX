833cb5a65e8353d3d8e9946184ef00f4b949dc00
Sriram Katta <sriram.katta@fau.com>
2025-07-26 19:45:35 +0200

    [MODIFY] Update fmt package configuration to remove Unnneded warnings

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -1,5 +1,9 @@
 CPMAddPackage(
   NAME fmt
+  GITHUB_REPOSITORY fmtlib/fmt
   GIT_TAG 11.2.0
-  GITHUB_REPOSITORY "fmtlib/fmt"
 )
+
+if(fmt_ADDED)
+  target_compile_options(fmt INTERFACE -diag-suppress=128,2417)
+endif()
