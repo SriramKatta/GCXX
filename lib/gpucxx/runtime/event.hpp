497615196544efa0607a49589220e9978c4e7636
Sriram Katta <sriram.katta@fau.com>
2025-08-27 10:38:35 +0200

    [ADD] missing include directives for event and stream headers

--- lib/gpucxx/runtime/event.hpp
+++ lib/gpucxx/runtime/event.hpp
@@ -2,6 +2,7 @@
 #ifndef GPUCXX_RUNTIME_EVENT_HPP_
 #define GPUCXX_RUNTIME_EVENT_HPP_
 
+#include <gpucxx/runtime/__flags/eventflags.hpp>
 #include <gpucxx/runtime/__event/event.hpp>
 #include <gpucxx/runtime/__event/event_ref.hpp>
 
