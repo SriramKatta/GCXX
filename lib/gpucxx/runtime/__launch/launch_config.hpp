b89770332eaffd9f14afef855ad55a519bda36b3
Sriram Katta <sriram.katta@fau.com>
2025-08-28 15:17:10 +0200

    ck1

--- lib/gpucxx/runtime/__launch/launch_config.hpp
+++ lib/gpucxx/runtime/__launch/launch_config.hpp
@@ -1,5 +1,14 @@
 #ifndef GPUCXX_RUNTIME_LAUNCH_LAUNCH_CONFIG_HPP_
 #define GPUCXX_RUNTIME_LAUNCH_LAUNCH_CONFIG_HPP_
 
+#include <gpucxx/backend/backend.hpp>
+
+class LaunchConfig{
+
+    private:
+        dim3 gridDim{1,1,1};
+        dim3 blockDim{1,1,1};
+}
+
 
 #endif
