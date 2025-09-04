27ae679067ec6101228647e02e7c2bcf221fca5b
Sriram Katta <sriram.katta@fau.com>
2025-09-04 19:28:17 +0200

    feat: add multi_gpu_testspace example with CMake configuration

---
+++ examples/multi_gpu_testspace/main.cpp
@@ -0,0 +1,14 @@
+#include <gpucxx/api.hpp>
+
+
+int main(int argc, char const* argv[]) {
+  int numdevices = 0;
+  GPUCXX_SAFE_RUNTIME_CALL(GetDeviceCount, (&numdevices));
+  
+  GPUCXX_SAFE_RUNTIME_CALL(SetDevice, (0));
+  auto str1 = gpucxx::Stream::Create();
+  GPUCXX_SAFE_RUNTIME_CALL(SetDevice, (1));
+  auto str2 = gpucxx::Stream::Create();
+  
+  return 0;
+}
