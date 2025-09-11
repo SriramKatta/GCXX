501dacbb644655024a191b9cafd86a34b5b19003
Sriram Katta <sriram.katta@fau.com>
2025-09-11 15:09:24 +0200

    [REFINE] adjust formatting and organization of backend.hpp for consistency

--- lib/gpucxx/backend/backend.hpp
+++ lib/gpucxx/backend/backend.hpp
@@ -2,20 +2,21 @@
 #ifndef GPUCXX_BACKEND_BACKEND_HPP_
 #define GPUCXX_BACKEND_BACKEND_HPP_
 
-// clang-format off
+
 #ifdef GPUCXX_CUDA_MODE
-    #include <gpucxx/backend/cuda_backend.hpp>
+#include <gpucxx/backend/cuda_backend.hpp>
 #elif GPUCXX_HIP_MODE
-    #include <gpucxx/backend/hip_backend.hpp>
+#include <gpucxx/backend/hip_backend.hpp>
 #endif
 
-#define STRINGIFY(x)                #x
-#define TOSTRING(x)                 STRINGIFY(x)
-#define STRINGIFY_AND_APPEND(a, b)  a##b
-#define APPEND_NAME(a, b)           STRINGIFY_AND_APPEND(a, b)
 
-#define        GPUCXX_RUNTIME_BACKEND(name)    APPEND_NAME(RUNTIME_BACKEND, name)
-constexpr auto GPUCXX_RUNTIME_BACKEND_STR    = TOSTRING(RUNTIME_BACKEND);
-// clang-format on
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+constexpr auto GPUCXX_RUNTIME_BACKEND_STR = TOSTRING(RUNTIME_BACKEND);
+
+
+#define STRINGIFY_AND_APPEND(a, b) a##b
+#define APPEND_NAME(a, b) STRINGIFY_AND_APPEND(a, b)
+#define GPUCXX_RUNTIME_BACKEND(name) APPEND_NAME(RUNTIME_BACKEND, name)
 
 #endif
