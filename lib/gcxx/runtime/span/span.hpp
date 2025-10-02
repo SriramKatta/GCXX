ba0400a9485cc608342ae4920df26ccbf6b2a790
Sriram Katta <sriram.katta@fau.com>
2025-10-02 17:31:21 +0200

    [REFINE] enhance span class with additional data and size utility functions for improved usability

--- lib/gcxx/runtime/span/span.hpp
+++ lib/gcxx/runtime/span/span.hpp
@@ -25,10 +25,9 @@ GCXX_NAMESPACE_DETAILS_BEGIN
 
 template <typename VT, std::size_t S>
 struct span_storage {
-  using pointer                     = VT*;
-  constexpr span_storage() noexcept = default;
+  GCXX_FHDC span_storage() noexcept = default;
 
-  constexpr span_storage(VT* v_ptr, std::size_t) noexcept : start(v_ptr) {}
+  GCXX_FHDC span_storage(VT* v_ptr, std::size_t) noexcept : start(v_ptr) {}
 
   VT* start{nullptr};
   static constexpr std::size_t size = S;
@@ -36,9 +35,9 @@ struct span_storage {
 
 template <typename VT>
 struct span_storage<VT, dynamic_extent> {
-  constexpr span_storage() noexcept = default;
+  GCXX_FHDC span_storage() noexcept = default;
 
-  constexpr span_storage(VT* v_ptr, std::size_t v_size) noexcept
+  GCXX_FHDC span_storage(VT* v_ptr, std::size_t v_size) noexcept
       : start(v_ptr), size(v_size) {}
 
   VT* start{nullptr};
@@ -53,6 +52,36 @@ struct is_complete<T, decltype(sizeof(T))> : std::true_type {};
 
 template <typename T>
 inline constexpr bool is_complete_v = is_complete<T>::value;
+
+template <class C>
+GCXX_FHDC auto data(C& c) -> decltype(c.data()) {
+  return c.data();
+}
+
+template <class C>
+GCXX_FHDC auto data(const C& c) -> decltype(c.data()) {
+  return c.data();
+}
+
+template <class T, std::size_t N>
+GCXX_FHDC T* data(T (&array)[N]) noexcept {
+  return array;
+}
+
+template <class E>
+GCXX_FHDC const E* data(std::initializer_list<E> il) noexcept {
+  return il.begin();
+}
+
+template <class C>
+GCXX_FHDC auto size(const C& c) -> decltype(c.size()) {
+  return c.size();
+}
+
+template <class T, std::size_t N>
+GCXX_FHDC std::size_t size(const T (&)[N]) noexcept {
+  return N;
+}
 
 GCXX_NAMESPACE_DETAILS_END
 
@@ -96,52 +125,54 @@ class span {
   // ==========================================================
   template <std::size_t E = Extent,
             typename std::enable_if_t<(E <= 0 || E == dynamic_extent), int> = 0>
-  GCXX_CXPR span() GCXX_NOEXCEPT {}
+  GCXX_CXPR GCXX_FHD span() GCXX_NOEXCEPT {}
 
-  GCXX_CXPR span(pointer ptr, size_type count) : storage_(ptr, count) {}
+  GCXX_CXPR GCXX_FHD span(pointer ptr, size_type count)
+      : storage_(ptr, count) {}
 
-  GCXX_CXPR span(pointer first_elem, pointer last_elem)
+  GCXX_CXPR GCXX_FHD span(pointer first_elem, pointer last_elem)
       : storage_(first_elem, last_elem - first_elem) {}
 
   template <std::size_t N, std::size_t E = Extent,
             typename std::enable_if_t<(E == dynamic_extent || E == N), int> = 0>
-  GCXX_CXPR span(element_type (&arr)[N]) GCXX_NOEXCEPT : storage_(arr, N) {}
+  GCXX_CXPR GCXX_FHD span(element_type (&arr)[N]) GCXX_NOEXCEPT
+      : storage_(arr, N) {}
 
   template <typename UVT, std::size_t N, std::size_t E = Extent,
             typename std::enable_if_t<(E == dynamic_extent || E == N), int> = 0>
-  GCXX_CXPR span(std::array<UVT, N>& arr) GCXX_NOEXCEPT
+  GCXX_CXPR GCXX_FHD span(std::array<UVT, N>& arr) GCXX_NOEXCEPT
       : storage_(arr.data(), N) {}
 
   template <typename UVT, std::size_t N, std::size_t E = Extent,
             typename std::enable_if_t<(E == dynamic_extent || E == N), int> = 0>
-  GCXX_CXPR span(const std::array<UVT, N>& arr) GCXX_NOEXCEPT
+  GCXX_CXPR GCXX_FHD span(const std::array<UVT, N>& arr) GCXX_NOEXCEPT
       : storage_(arr.data(), N) {}
 
   template <typename container, std::size_t E = Extent,
             typename std::enable_if_t<(E == dynamic_extent), int> = 0>
-  GCXX_CXPR span(const container& arr) GCXX_NOEXCEPT
-      : storage_(std::data(arr), std::size(arr)) {}
+  GCXX_CXPR GCXX_FHD span(const container& arr) GCXX_NOEXCEPT
+      : storage_(details_::data(arr), details_::size(arr)) {}
 
   template <typename container, std::size_t E = Extent,
             typename std::enable_if_t<(E == dynamic_extent), int> = 0>
-  GCXX_CXPR span(container& arr) GCXX_NOEXCEPT
-      : storage_(std::data(arr), std::size(arr)) {}
+  GCXX_CXPR GCXX_FHD span(container& arr) GCXX_NOEXCEPT
+      : storage_(details_::data(arr), details_::size(arr)) {}
 
   template <typename OtherElementType, std::size_t OtherExtent,
             typename std::enable_if_t<(Extent == dynamic_extent ||
                                        OtherExtent == dynamic_extent ||
                                        Extent == OtherExtent),
                                       int> = 0>
-  GCXX_CXPR span(const span<OtherElementType, OtherExtent>& other) GCXX_NOEXCEPT
-      : storage_(other.data(), other.size()) {}
+  GCXX_CXPR GCXX_FHD span(const span<OtherElementType, OtherExtent>& other)
+    GCXX_NOEXCEPT : storage_(other.data(), other.size()) {}
 
-  GCXX_CXPR span(const span& other) GCXX_NOEXCEPT = default;
+  GCXX_CXPR GCXX_FHD span(const span& other) GCXX_NOEXCEPT = default;
 
   // ==========================================================
   //                         destructor
   // ==========================================================
 
-  ~span() = default;
+  GCXX_CXPR GCXX_FHD span() = default;
 
   // ==========================================================
   //                         operator =
@@ -153,17 +184,17 @@ class span {
   //                         Iterators
   // ==========================================================
 
-  GCXX_CXPR auto begin() GCXX_CONST_NOEXCEPT -> iterator { return data(); }
+  GCXX_FHDC auto begin() GCXX_CONST_NOEXCEPT -> iterator { return data(); }
 
-  GCXX_CXPR auto end() GCXX_CONST_NOEXCEPT -> iterator {
+  GCXX_FHDC auto end() GCXX_CONST_NOEXCEPT -> iterator {
     return data() + size();
   }
 
-  GCXX_CXPR auto rbegin() GCXX_CONST_NOEXCEPT -> reverse_iterator {
+  GCXX_FH GCXX_CXPR auto rbegin() GCXX_CONST_NOEXCEPT -> reverse_iterator {
     return reverse_iterator(end());
   }
 
-  GCXX_CXPR auto rend() GCXX_CONST_NOEXCEPT -> reverse_iterator {
+  GCXX_FH GCXX_CXPR auto rend() GCXX_CONST_NOEXCEPT -> reverse_iterator {
     return reverse_iterator(begin());
   }
 
@@ -171,15 +202,17 @@ class span {
   //                       Element access
   // ==========================================================
 
-  GCXX_CXPR auto front() const -> reference { return *begin(); }
+  GCXX_FHDC auto front() const -> reference { return *begin(); }
 
-  GCXX_CXPR auto back() const -> reference { return *rbegin(); }
+  GCXX_FHDC auto back() const -> reference {
+    return *(end() - 1);
+  }  //*rbegin() cant be used since operator * in host only
 
-  GCXX_CXPR auto operator[](size_type idx) const -> reference {
+  GCXX_FHDC auto operator[](size_type idx) const -> reference {
     return *(data() + idx);
   }
 
-  GCXX_CXPR auto data() GCXX_CONST_NOEXCEPT -> pointer {
+  GCXX_FHDC auto data() GCXX_CONST_NOEXCEPT -> pointer {
     return storage_.start;
   }
 
@@ -187,11 +220,11 @@ class span {
   //                         Observers
   // ==========================================================
 
-  GCXX_CXPR auto size() GCXX_CONST_NOEXCEPT -> size_type {
+  GCXX_FHDC auto size() GCXX_CONST_NOEXCEPT -> size_type {
     return storage_.size;
   }
 
-  GCXX_CXPR auto size_bytes() GCXX_CONST_NOEXCEPT -> size_type {
+  GCXX_FHDC auto size_bytes() GCXX_CONST_NOEXCEPT -> size_type {
     return size() * sizeof(element_type);
   }
 
@@ -202,21 +235,21 @@ class span {
   // ==========================================================
 
   template <std::size_t Count>
-  GCXX_CXPR auto first() const -> span<element_type, Count> {
+  GCXX_FHDC auto first() const -> span<element_type, Count> {
     return {data(), Count};
   }
 
-  GCXX_CXPR auto first(size_type count) const
+  GCXX_FHDC auto first(size_type count) const
     -> span<element_type, dynamic_extent> {
     return {data(), count};
   }
 
   template <std::size_t Count>
-  GCXX_CXPR auto last() const -> span<element_type, Count> {
+  GCXX_FHDC auto last() const -> span<element_type, Count> {
     return {data() + (size() - Count), Count};
   }
 
-  GCXX_CXPR auto last(size_type count) const
+  GCXX_FHDC auto last(size_type count) const
     -> span<element_type, dynamic_extent> {
     return {data() + (size() - count), count};
   }
@@ -229,11 +262,11 @@ class span {
            : (Extent != Count ? Extent - Offset : dynamic_extent)>;
 
   template <std::size_t Offset, std::size_t Count = dynamic_extent>
-  constexpr auto subspan() const -> subspan_return_t<Offset, Count> {
+  GCXX_FHDC auto subspan() const -> subspan_return_t<Offset, Count> {
     return {data() + Offset, Count != dynamic_extent ? Count : size() - Offset};
   }
 
-  constexpr auto subspan(size_type offset,
+  GCXX_FHDC auto subspan(size_type offset,
                          size_type count = dynamic_extent) const
     -> span<element_type> {
     return {data() + offset, count == dynamic_extent ? size() - count : count};
@@ -248,6 +281,26 @@ class span {
  private:
   storage_type storage_{};
 };
+
+// █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+// █                    Deduction guides                    █
+// █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+template <class VT, size_t N>
+span(VT (&)[N]) -> span<VT, N>;
+
+template <class VT, size_t N>
+span(std::array<VT, N>&) -> span<VT, N>;
+
+template <class VT, size_t N>
+span(const std::array<VT, N>&) -> span<const VT, N>;
+
+template <class Container>
+span(Container&) -> span<typename std::remove_reference_t<
+  decltype(*std::data(std::declval<Container&>()))>>;
+
+template <class Container>
+span(const Container&) -> span<const typename Container::value_type>;
+
 
 GCXX_NAMESPACE_MAIN_END
 
