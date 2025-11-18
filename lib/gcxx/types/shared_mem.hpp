e48bc69ed112c6bb07f85e787b748ebe284eabd7
Sriram Katta <sriram.katta@fau.de>
2025-11-18 14:59:14 +0100

    refactor: rename SharedMemory to dynamicSharedMemory and update memory access

--- lib/gcxx/types/shared_mem.hpp
+++ lib/gcxx/types/shared_mem.hpp
@@ -2,35 +2,50 @@
 #ifndef GCXX_TYPES_SHARED_MEM_HPP
 #define GCXX_TYPES_SHARED_MEM_HPP
 
+#include <cstddef>
 
 #include <gcxx/backend/backend.hpp>
 #include <gcxx/macros/define_macros.hpp>
 
+
 GCXX_NAMESPACE_MAIN_BEGIN
 
 template <typename VT>
-struct SharedMemory {
+struct dynamicSharedMemory {
   __device__ inline operator VT*() {
-    extern __shared__ std::byte __smem[];
-    return (VT*)__smem;
+    extern __shared__ char m_smem[];
+    return (VT*)m_smem;
   }
 
   __device__ inline operator const VT*() const {
-    extern __shared__ std::byte __smem[];
-    return (VT*)__smem;
+    extern __shared__ char m_smem[];
+    return (VT*)m_smem;
   }
 };
 
 template <>
-struct SharedMemory<double> {
+struct dynamicSharedMemory<double> {
   __device__ inline operator double*() {
-    extern __shared__ std::byte __smem[];
-    return (double*)__smem;
+    extern __shared__ char m_smem[];
+    return (double*)m_smem;
   }
 
   __device__ inline operator const double*() const {
-    extern __shared__ std::byte __smem[];
-    return (double*)__smem;
+    extern __shared__ char m_smem[];
+    return (double*)m_smem;
+  }
+};
+
+template <typename VT, std::size_t N>
+struct staticSharedArray {
+  __device__ inline operator VT*() {
+    extern __shared__ VT m_smem[];
+    return (VT*)m_smem;
+  }
+
+  __device__ inline operator const VT*() const {
+    extern __shared__ VT m_smem[];
+    return (VT*)m_smem;
   }
 };
 
