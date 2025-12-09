b4b1ffe64d1838d6c96eaf26b10ec24137506b61
Sriram Katta <sriram.katta@fau.de>
2025-12-09 23:09:50 +0100

    refactor: update include guards and formatting in launch_config.hpp

--- lib/gcxx/runtime/launch/launch_config.hpp
+++ lib/gcxx/runtime/launch/launch_config.hpp
@@ -1,14 +1,22 @@
-#ifndef GCXX_RUNTIME_LAUNCH_LAUNCH_CONFIG_HPP_
-#define GCXX_RUNTIME_LAUNCH_LAUNCH_CONFIG_HPP_
+#pragma once
+#ifndef GCXX_RUNTIME_LAUNCH_LAUNCH_CONFIG_HPP
+#define GCXX_RUNTIME_LAUNCH_LAUNCH_CONFIG_HPP
 
 #include <gcxx/backend/backend.hpp>
+#include <gcxx/macros/define_macros.hpp>
+#include <gcxx/runtime/stream/stream_wrap.hpp>
+
+GCXX_NAMESPACE_MAIN_BEGIN
 
 class LaunchConfig {
-
+ public:
  private:
-  // dim3 gridDim{1, 1, 1};
-  // dim3 blockDim{1, 1, 1};
-}
+  dim3 gridDim{1, 1, 1};
+  dim3 blockDim{1, 1, 1};
+  std::size_t smem_bytes{0};
+  stream_wrap stream;
+};
 
+GCXX_NAMESPACE_MAIN_END
 
 #endif
