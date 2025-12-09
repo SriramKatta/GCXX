9e06f22074cb03d836ecb9b7ed6ea5ff83cdbd0a
Sriram Katta <sriram.katta@fau.de>
2025-12-09 23:38:53 +0100

    refactor: reorder include directives in memory.hpp

--- lib/gcxx/runtime/memory.hpp
+++ lib/gcxx/runtime/memory.hpp
@@ -3,10 +3,10 @@
 #define GCXX_RUNTIME_MEMORY_HPP_
 
 #include <gcxx/runtime/memory/copy.hpp>
-#include <gcxx/runtime/memory/span/span.hpp>
-#include <gcxx/runtime/memory/vector/host_vector.hpp>
-#include <gcxx/runtime/memory/vector/device_vector.hpp>
 #include <gcxx/runtime/memory/smartpointers/pointers.hpp>
+#include <gcxx/runtime/memory/span/span.hpp>
+#include <gcxx/runtime/memory/vector/device_vector.hpp>
+#include <gcxx/runtime/memory/vector/host_vector.hpp>
 
 
 #endif
