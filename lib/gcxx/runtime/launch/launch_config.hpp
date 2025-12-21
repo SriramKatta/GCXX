a4fe2748dc1f10dbdb85d9c805471c8b098da148
Sriram Katta <sriram.katta@fau.de>
2025-12-21 18:52:28 +0100

    feat: include StreamView in LaunchConfig and update constructor initialization

--- lib/gcxx/runtime/launch/launch_config.hpp
+++ lib/gcxx/runtime/launch/launch_config.hpp
@@ -5,6 +5,8 @@
 #include <gcxx/backend/backend.hpp>
 #include <gcxx/macros/define_macros.hpp>
 
+#include <gcxx/runtime/stream/stream_view.hpp>
+
 GCXX_NAMESPACE_MAIN_DETAILS_BEGIN
 using deviceLaunchConfig_t = GCXX_RUNTIME_BACKEND(LaunchConfig_t);
 GCXX_NAMESPACE_MAIN_DETAILS_END
@@ -14,12 +16,20 @@ GCXX_NAMESPACE_MAIN_BEGIN
 
 class LaunchConfig {
  private:
-  using deviceLaunchConfig_t  = details_::deviceLaunchConfig_t;
-  deviceLaunchConfig_t config = {0};
+  using deviceLaunchConfig_t   = details_::deviceLaunchConfig_t;
+  deviceLaunchConfig_t config_ = {0};
 
  public:
+  LaunchConfig(dim3 griddim = {1, 1, 1}, dim3 blockdim = {1, 1, 1},
+               std::size_t smemBytes = 0,
+               const StreamView& sv  = details_::NULL_STREAM)
+      : config_({griddim, blockdim, smemBytes, sv.getRawStream(), nullptr, 0}) {
+  }
+
+  void print() {}
 };
 
 GCXX_NAMESPACE_MAIN_END
+
 
 #endif
