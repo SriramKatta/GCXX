d379a815627f399f893b7b6e1837d5490d7c0db7
Sriram Katta <sriram.katta@fau.de>
2025-12-28 03:40:23 +0100

    refactor: include graph_host_node_params in graph.hpp for improved parameter management

--- lib/gcxx/runtime/graph.hpp
+++ lib/gcxx/runtime/graph.hpp
@@ -6,6 +6,7 @@
 
 
 #include <gcxx/runtime/graph/params/graph_kernel_node_params.hpp>
+#include <gcxx/runtime/graph/params/graph_host_node_params.hpp>
 #include <gcxx/runtime/graph/params/graph_memcpy3d_params.hpp>
 #include <gcxx/runtime/graph/params/graph_memset_params.hpp>
 
