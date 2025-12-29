938a44eae744c7e20471c9e7313642da20aa6856
Sriram Katta <sriram.katta@fau.de>
2025-12-29 19:06:48 +0100

    feat: add graph_params.hpp for graph parameter definitions

---
+++ lib/gcxx/runtime/graph/graph_params.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef GCXX_RUNTIME_GRAPH_GRAPH_PARAMS_HPP_
+#define GCXX_RUNTIME_GRAPH_GRAPH_PARAMS_HPP_
+
+#include <gcxx/runtime/graph/params/graph_host_node_params.hpp>
+#include <gcxx/runtime/graph/params/graph_kernel_node_params.hpp>
+#include <gcxx/runtime/graph/params/graph_memcpy3d_params.hpp>
+#include <gcxx/runtime/graph/params/graph_memset_params.hpp>
+
+#endif
