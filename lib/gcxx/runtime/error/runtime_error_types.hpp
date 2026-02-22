39a4cfb906ca7dacf4058e5d897871bbb2d7fabf
Sriram Katta <sriram.katta@fau.de>
2026-02-22 17:35:16 +0100

    fix: update GetErrorName and GetErrorString return types to const char*

--- lib/gcxx/runtime/error/runtime_error_types.hpp
+++ lib/gcxx/runtime/error/runtime_error_types.hpp
@@ -14,11 +14,11 @@ using deviceError_t              = GCXX_RUNTIME_BACKEND(Error_t);
 GCXX_CXPR auto deviceErrSuccess  = GCXX_RUNTIME_BACKEND(Success);
 GCXX_CXPR auto deviceErrNotReady = GCXX_RUNTIME_BACKEND(ErrorNotReady);
 
-GCXX_FH std::string_view GetErrorName(deviceError_t err) {
+GCXX_FHD const char* GetErrorName(deviceError_t err) {
   return GCXX_RUNTIME_BACKEND(GetErrorName)(err);
 }
 
-GCXX_FH std::string_view GetErrorString(deviceError_t err) {
+GCXX_FHD const char* GetErrorString(deviceError_t err) {
   return GCXX_RUNTIME_BACKEND(GetErrorString)(err);
 }
 
@@ -26,12 +26,12 @@ GCXX_FH std::string make_message(deviceError_t err, std::string_view context) {
   std::string msg;
   msg.reserve(context.size() + 128);
 
-  msg.append(context);
-  msg.append(": ");
-  msg.append(GetErrorName(err));
-  msg.append(" (");
-  msg.append(GetErrorString(err));
-  msg.append(")");
+  msg.append(context)
+    .append(": ")
+    .append(GetErrorName(err))
+    .append(" (")
+    .append(GetErrorString(err))
+    .append(")");
 
   return msg;
 }
