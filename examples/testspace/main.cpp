03cf7cf65c08f5a55f1e57e5478f480f0e1dfd14
Sriram Katta <sriram.katta@fau.de>
2026-03-03 12:00:47 +0100

    fix: update print_float_int function to use fmt library for formatting and improve template requirements

--- examples/testspace/main.cpp
+++ examples/testspace/main.cpp
@@ -3,16 +3,23 @@
 #include <gcxx/api.hpp>
 #include <type_traits>
 
-GCXX_TEMPLATE(class VT)
-GCXX_REQUIRES((std::is_arithmetic_v<VT>) GCXX_AND (std::is_floating_point_v<VT> ||
-               std::is_integral_v<VT>))
+GCXX_TEMPLATE(typename VT)
+GCXX_REQUIRES(std::is_arithmetic_v<VT> GCXX_AND std::is_floating_point_v<VT> ||
+              std::is_integral_v<VT>)
 
 void print_float_int(const VT& val) {
-  std::cout << val << std::endl;
+  fmt::print("{}\n", val);
+}
+
+template <typename VT>
+auto reciprocal(const VT& value) GCXX_TRAILING_REQUIRES(VT)(
+  std::is_floating_point_v<VT>&& std::is_arithmetic_v<VT>) {
+  return VT{1} / value;
 }
 
 int main() {
   print_float_int(3);
   print_float_int(3.2);
-  // print_float_int('c');
+  reciprocal(3.2);
+  // reciprocal(3);
 }
