6d76b006313f9f02b9dc1493dfecd654ff374836
Sriram Katta <sriram.katta@fau.de>
2026-04-12 02:05:14 +0200

    feat: refactor main.cpp to include linear span printing and update mdspan usage

--- examples/by_api_module/mdspan_htu/main.cpp
+++ examples/by_api_module/mdspan_htu/main.cpp
@@ -3,11 +3,19 @@
 #include <fmt/chrono.h>  // needed to print the chrono durations
 #include <fmt/format.h>
 #include <array>
-#include <gcxx/api.hpp>
+#include <gcxx/runtime/memory/spans/spans.hpp>
 #include <numeric>
 
 constexpr int nx = 5;
 constexpr int ny = 5;
+
+template <typename VT, std::size_t N>
+void print_linear(const gcxx::span<VT, N>& sps) {
+  for (auto& i : sps) {
+    fmt::print("{:3d} ", i);
+  }
+  fmt::print("\n");
+}
 
 template <typename VT, typename EXT, typename LAY, typename acc>
 void print(const gcxx::mdspan<VT, EXT, LAY, acc>& span) {
@@ -22,9 +30,24 @@ void print(const gcxx::mdspan<VT, EXT, LAY, acc>& span) {
 int main(int argc, char const* argv[]) {
   std::array<int, nx * ny> vec;
   std::iota(vec.begin(), vec.end(), 1);
+
+  gcxx::span lin_span(vec);
+
+  fmt::print("linear data : \n");
+  print_linear(lin_span);
+
   gcxx::mdspan<int, gcxx::dextents<int, 2>, gcxx::layout_left,
                gcxx::restrict_accessor<gcxx::default_accessor<int>>>
     span(vec.data(), nx, ny);
+
+  fmt::print("\nmdspan layout left data : \n");
   print(span);
+
+  gcxx::mdspan<int, gcxx::dextents<int, 2>, gcxx::layout_right,
+               gcxx::default_accessor<int>>
+    span2(vec.data(), nx, ny);
+
+  fmt::print("\nmdspan layout right data : \n");
+  print(span2);
   return 0;
 }
