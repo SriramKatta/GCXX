42b29201730e4c2779cdafa0323806c29ba7d2a9
Sriram Katta <sriram.katta.939@gmail.com>
2026-05-14 06:52:06 +0000

    update based on new ctors

--- examples/by_api_module/stream_wrapper/main.cpp
+++ examples/by_api_module/stream_wrapper/main.cpp
@@ -7,10 +7,10 @@ int main() {
   namespace gflags = gcxx::flags;
   gcxx::Stream str1;
   gcxx::Stream str2(gflags::streamType::NoSyncWithNull);
-  auto str1default = gcxx::Stream::Create(gflags::streamType::SyncWithNull);
-  auto str3        = gcxx::Stream::Create(gflags::streamType::NoSyncWithNull);
-  auto str4        = gcxx::Stream::Create(gflags::streamType::NullStream,
-                                          gflags::streamPriority::VeryHigh);
+  auto str1default = gcxx::Stream(gflags::streamType::SyncWithNull);
+  auto str3        = gcxx::Stream(gflags::streamType::NoSyncWithNull);
+  auto str4        = gcxx::Stream(gflags::streamType::NullStream,
+                                  gflags::streamPriority::VeryHigh);
 
   std::cout << "size of stream obj " << sizeof(str1) << std::endl;
   std::cout << "size of stream obj " << sizeof(str1.get()) << std::endl;
