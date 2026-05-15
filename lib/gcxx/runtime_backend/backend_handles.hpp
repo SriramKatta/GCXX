7ca2be950cde8c9ee3a623bfea5ed1c420ea1ff2
Sriram Katta <sriram.katta.939@gmail.com>
2026-05-15 22:06:01 +0000

    style: enhance code readability by adding section headers for error, stream, event, and graph handles

--- lib/gcxx/runtime_backend/backend_handles.hpp
+++ lib/gcxx/runtime_backend/backend_handles.hpp
@@ -8,11 +8,16 @@
 
 GCXX_NAMESPACE_MAIN_DRIVER_BEGIN
 
+// ╔════════════════════════════════════════════════════════╗
+// ║                     Error handles                      ║
+// ╚════════════════════════════════════════════════════════╝
 using deviceError_t                     = GCXX_RUNTIME_BACKEND(Error_t);
 GCXX_CXPR inline auto deviceErrSuccess  = GCXX_RUNTIME_BACKEND(Success);
 GCXX_CXPR inline auto deviceErrNotReady = GCXX_RUNTIME_BACKEND(ErrorNotReady);
 
-
+// ╔════════════════════════════════════════════════════════╗
+// ║                     Sream Handles                      ║
+// ╚════════════════════════════════════════════════════════╝
 using deviceStream_t = GCXX_RUNTIME_BACKEND(Stream_t);
 inline static constexpr deviceStream_t NULL_STREAM{nullptr};  // NOLINT
 
@@ -20,10 +25,17 @@ inline static constexpr deviceStream_t NULL_STREAM{nullptr};  // NOLINT
 inline static const auto INVALID_STREAM = reinterpret_cast<deviceStream_t>(~0ULL);  // NOLINT
 // clang-format on
 
+
+// ╔════════════════════════════════════════════════════════╗
+// ║                     Event handles                      ║
+// ╚════════════════════════════════════════════════════════╝
 using deviceEvent_t = GCXX_RUNTIME_BACKEND(Event_t);
 inline static constexpr deviceEvent_t INVALID_EVENT{nullptr};  // NOLINT
 
 
+// ╔════════════════════════════════════════════════════════╗
+// ║                     Graph handles                      ║
+// ╚════════════════════════════════════════════════════════╝
 using deviceGraph_t     = GCXX_RUNTIME_BACKEND(Graph_t);
 using deviceGraphNode_t = GCXX_RUNTIME_BACKEND(GraphNode_t);
 using deviceGraphExec_t = GCXX_RUNTIME_BACKEND(GraphExec_t);
