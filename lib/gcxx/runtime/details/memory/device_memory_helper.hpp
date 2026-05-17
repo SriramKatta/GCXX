45181de56509f316a4beb88ab99a54308fc0ef32
Sriram Katta <sriram.katta.939@gmail.com>
2026-05-17 13:31:08 +0000

    fix: correct typo in device_free_async error message

--- lib/gcxx/runtime/details/memory/device_memory_helper.hpp
+++ lib/gcxx/runtime/details/memory/device_memory_helper.hpp
@@ -51,10 +51,10 @@ GCXX_CXPR auto device_free = [](void* ptr) {
   GCXX_SAFE_RUNTIME_CALL(Free, "Failed to deallocate device memory", ptr);
 };
 
-GCXX_CXPR auto device_free_async = [](void* ptr, const StreamView& sv =
-                                                   driver::NULL_STREAM) {
+GCXX_CXPR auto device_free_async = [](void* ptr,
+                                       const StreamView& sv = StreamView::Null()) {
   GCXX_SAFE_RUNTIME_CALL(FreeAsync,
-                         "Failed to deallocate device memory Asynchronysly",
+                         "Failed to deallocate device memory asynchronously",
                          ptr, sv.getRawStream());
 };
 
