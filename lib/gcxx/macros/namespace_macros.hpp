3748907ad991b8cf139ba47263a4db7da8dd05c3
Sriram Katta <sriram.katta.939@gmail.com>
2026-05-20 17:51:56 +0000

    [update] seprate NAMESPACE_*_END into its own ifdef gaurds

--- lib/gcxx/macros/namespace_macros.hpp
+++ lib/gcxx/macros/namespace_macros.hpp
@@ -8,6 +8,9 @@
 #define GCXX_NAMESPACE_MAIN_BEGIN \
   namespace gcxx {                \
     inline namespace v1 {
+#endif
+
+#ifndef GCXX_NAMESPACE_MAIN_END
 #define GCXX_NAMESPACE_MAIN_END \
   } /* inline namespace v1  */  \
   }  // namespace gcxx
@@ -15,24 +18,35 @@
 
 #ifndef GCXX_NAMESPACE_DETAILS_BEGIN
 #define GCXX_NAMESPACE_DETAILS_BEGIN namespace details_ {
+#endif
+
+#ifndef GCXX_NAMESPACE_DETAILS_END
 #define GCXX_NAMESPACE_DETAILS_END } /* namespace details_  */
 #endif
 
 #ifndef GCXX_NAMESPACE_DRIVER_BEGIN
 #define GCXX_NAMESPACE_DRIVER_BEGIN namespace driver {
+#endif
+
+#ifndef GCXX_NAMESPACE_DRIVER_END
 #define GCXX_NAMESPACE_DRIVER_END } /* namespace driver  */
 #endif
 
 #ifndef GCXX_NAMESPACE_FLAGS_BEGIN
 #define GCXX_NAMESPACE_FLAGS_BEGIN namespace flags {
-#define GCXX_NAMESPACE_FLAGS_END } /* namespace flags  */
 #endif
 
+#ifndef GCXX_NAMESPACE_FLAGS_END
+#define GCXX_NAMESPACE_FLAGS_END } /* namespace flags  */
+#endif
 
 #ifndef GCXX_NAMESPACE_MAIN_DETAILS_BEGIN
 #define GCXX_NAMESPACE_MAIN_DETAILS_BEGIN \
   GCXX_NAMESPACE_MAIN_BEGIN               \
   GCXX_NAMESPACE_DETAILS_BEGIN
+#endif
+
+#ifndef GCXX_NAMESPACE_MAIN_DETAILS_END
 #define GCXX_NAMESPACE_MAIN_DETAILS_END \
   GCXX_NAMESPACE_DETAILS_END            \
   GCXX_NAMESPACE_MAIN_END
@@ -42,6 +56,9 @@
 #define GCXX_NAMESPACE_MAIN_FLAGS_BEGIN \
   GCXX_NAMESPACE_MAIN_BEGIN             \
   GCXX_NAMESPACE_FLAGS_BEGIN
+#endif
+
+#ifndef GCXX_NAMESPACE_MAIN_FLAGS_END
 #define GCXX_NAMESPACE_MAIN_FLAGS_END \
   GCXX_NAMESPACE_FLAGS_END            \
   GCXX_NAMESPACE_MAIN_END
@@ -51,6 +68,9 @@
 #define GCXX_NAMESPACE_MAIN_DRIVER_BEGIN \
   GCXX_NAMESPACE_MAIN_BEGIN              \
   GCXX_NAMESPACE_DRIVER_BEGIN
+#endif
+
+#ifndef GCXX_NAMESPACE_MAIN_DRIVER_END
 #define GCXX_NAMESPACE_MAIN_DRIVER_END \
   GCXX_NAMESPACE_DRIVER_END            \
   GCXX_NAMESPACE_MAIN_END
