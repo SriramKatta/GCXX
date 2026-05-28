95f01e7858410baa993a8a7c79c3a6fc2d0632a0
Sriram Katta <sriram.katta.939@gmail.com>
2026-05-28 08:26:36 +0000

    Move unneccesy compile flags into folder to reduce number of passed args to compiler

--- lib/gcxx/runtime/memory/spans/mdspan/mdspan.hpp
+++ lib/gcxx/runtime/memory/spans/mdspan/mdspan.hpp
@@ -3,7 +3,16 @@
 #pragma once
 #ifndef GCXX_RUNTIME_MEMORY_MDSPAN_MDSPAN_HPP
 #define GCXX_RUNTIME_MEMORY_MDSPAN_MDSPAN_HPP
+
+#define MDSPAN_IMPL_STANDARD_NAMESPACE gcxx
+#define MDSPAN_IMPL_HAS_CUDA 1
+#define MDSPAN_IMPL_HAS_HIP 1
+
 #include <mdspan.hpp>
+
+#undef MDSPAN_IMPL_STANDARD_NAMESPACE
+#undef MDSPAN_IMPL_HAS_CUDA
+#undef MDSPAN_IMPL_HAS_HIP
 
 
 #include <gcxx/runtime/memory/spans/mdspan/resrict_accessor.hpp>
