e5a0f8a3f5bd738a060b883e562b079569cee114
Sriram Katta <sriram.katta.939@gmail.com>
2026-07-29 20:03:08 +0000

    Refactor CMake files to improve formatting and readability for fmt and GTest configurations

--- examples/cmake/fmt.cmake
+++ examples/cmake/fmt.cmake
@@ -8,7 +8,8 @@ if(fmt_ADDED)
   # Treat fmt's headers as system headers (so they don't warn in our code)
   # and silence warnings from compiling fmt's own sources.
   get_target_property(FMT_INCLUDES fmt INTERFACE_INCLUDE_DIRECTORIES)
-  set_target_properties(fmt PROPERTIES
-    INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${FMT_INCLUDES}")
+  set_target_properties(
+    fmt PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${FMT_INCLUDES}"
+  )
   target_compile_options(fmt PRIVATE -w)
 endif()
