a366c2201f46dbe61736a72e6a84d03de9045c5d
Sriram Katta <sriram.katta.939@gmail.com>
2026-08-10 13:24:36 +0200

    Remove redundant comment in backend.hpp regarding CUDA and HIP backend handles

--- lib/gcxx/backend/backend.hpp
+++ lib/gcxx/backend/backend.hpp
@@ -32,7 +32,6 @@ constexpr auto GCXX_RUNTIME_BACKEND_STR = TOSTRING(RUNTIME_BACKEND);
 #define GCXX_ATTRIBUTE_BACKEND(name) APPEND_NAME(ATTRIBUTE_BACKEND, name)
 
 // Macro to handle Backend handles with different names between CUDA and HIP
-// still starting with cuda/hip
 #if GCXX_CUDA_MODE()
 #define GCXX_DIRECT_BACKEND_ALT(cuda_name, hip_name) cuda_name
 #elif GCXX_HIP_MODE()
